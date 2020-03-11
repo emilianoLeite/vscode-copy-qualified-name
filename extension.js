fafbf9be6ac1c322a7acd5b0b733cb2a620997cb
Emiliano Leite <leiteemiliano@gmail.com>
2020-03-11 09:29:44 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -20,9 +20,6 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
-
-
-    // Display a message box to the user
     const currentDocument = vscode.window.activeTextEditor.document
     const namespaceDefinitions = []
 
